c95a2427d063498eb092304a63083b9a99ec1a46
soyoung-ahn <89114528+soyoung-ahn@users.noreply.github.com>
2021-08-31 10:47:02 +0900

    Update variables.tf

--- variables.tf
+++ variables.tf
@@ -1,6 +1,6 @@
  variable "ec2_name" {
    description = "ec2 instance tag Name"
-   default = "QA > Terraform_Provisioning (core)"
+   default = "QA_modify > Terraform_Provisioning (core)"
  }
 
  variable "subnet_id" {
